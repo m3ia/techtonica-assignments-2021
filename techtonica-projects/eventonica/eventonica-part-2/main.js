9ceb61c26eeb4304c38b83a0ccf6c91224173521
Meia Natividad <meianatividad@gmail.com>
2021-02-21 17:49:53 -0800

    complete models.js code

--- techtonica-projects/eventonica/eventonica-part-2/main.js
+++ techtonica-projects/eventonica/eventonica-part-2/main.js
@@ -2,12 +2,14 @@
  * Add all your DOM event handlers and other UI code in this file.
  */
 
+
+const newProp = // document.getElementById("userUpdateProp").value. need to create this in html at some point
+
 document.addEventListener("DOMContentLoaded", () => {
   // Use this to call all the logic we already created
   const app = new Eventonica();
 
-  // Builds HTML list for all event. You must call this function after you
-  // change, add, or remove any events.
+  // Builds HTML list for all event. You must call this function after you change, add, or remove any events.
   const refreshEventsList = () => {
     document.querySelector("#events-list").innerHTML = Event.all
       .map((event) => `<li>${event.name}</li>`)
@@ -21,9 +23,32 @@ document.addEventListener("DOMContentLoaded", () => {
   addEventForm.addEventListener("submit", (submitEvent) => {
     submitEvent.preventDefault();
     const name = document.querySelector("#add-event-name").value;
-    const event = app.addEvent(name);
+    const event = app.addEvent(name, /*'date', ['category1', 'category2']*/);
     console.log("Added event", event);
     refreshEventsList();
     addEventForm.reset();
   });
 });
+
+// document.addEventListener("DOMContentLoaded", () => {
+//   const newUser = new User();
+
+//   // Builds HTML list for all event. You must call this function after you change, add, or remove any events.
+//   const refreshUserList = () => {
+//       .map((user) => `<li>${user.name}</li>`)
+//       .join("\n");
+//   };
+
+
+//   const addUserForm = document.querySelector("#add-user");
+
+//   // Handle add event form submit by calling our instance of Eventonica, `app`
+//   addUserForm.addEventListener("submit", (submitUser) => {
+//     submitUser.preventDefault();
+//     const userName = document.querySelector("#add-user-name").value;
+//     const user = newUser.addUser(userName);
+//     console.log("Added user", user);
+//     refreshUserList();
+//     addUserForm.reset();
+//   });
+// });
--- techtonica-projects/eventonica/eventonica-part-2/models.js
+++ techtonica-projects/eventonica/eventonica-part-2/models.js
@@ -3,39 +3,31 @@
  * (The following is a just a small subset for the Add Event form on the example page to work)
  */
 
-const user_Name = document.getElementById("add-user-name").value;
+const user_Name = document.getElementById("add-username").value;
 
 class Event {
   static all = [];
   static _nextId = 100;
 
-  constructor(name, location, startDate, endDate, startTime, endTime, desc, price, categories) {
+  constructor(name, date, categories) {
     this.name = name;
     this.id = Event._nextId++;
-    this.location = location;
-    this.startDate = startDate;
-    this.endDAte = endDate;
-    this.startTime = startTime;
-    this.endTime = endTime;
-    this.desc = desc;
-    this.price = price;
-    this.categories = categories;
+    this.date = date;
+    this.categories = categories; // entertainment? public? food? 
 
     Event.all.push(this); // keep track of all created instances
 
-        // properties: event name, place/link, time, description, organizer, category (career? presentation? live show? entertainment? group)
-    // decide what properties are required
-
   }
 
-  static findByDate(/* user input (date) and click */) {
+  static findByDate(date) {
+    //need to declare a searchDate var
     // searches over constructor objects for events that have dates that occur on this set date. 
-    return [];
+    return Event.all.filter(event => event.date === date);
   }
 
-  static findByCategory(/* checkbox input / click  */) {
+  static findByCategory(category) {
     // searches for events that check a category array for each event object for the selected category
-    return [];
+    return Event.all.filter(event => event.categories.includes(category));
   }
 }
 
@@ -43,13 +35,9 @@ class User {
   static all = [];
   static _nextId = 200;
 
-  constructor(firstName, lastName, username, email, password) {
-    this.firstName = firstName;
-    this.lastName = lastName;
+  constructor(name) {
+    this.name = name;
     this.id = User._nextId++;
-    this.username = username;
-    this.email = _email;
-    this.password = _password;
     // decide what properties are required on an instance
     User.all.push(this); // keep track of all created instances
   }
@@ -63,7 +51,7 @@ class User {
 }
 
 class Eventonica {
-  addEvent(name, location, startDate, endDate, startTime, endTime, desc, price, categories) {
+  addEvent(name, date, categories) {
     // have a field for each piece of input
     // create a function so user can enter input
     // create a submit button. onlick, will erase everything, parse all input, and enter it into an object for the user.
@@ -71,7 +59,7 @@ class Eventonica {
 
     // Adds a new Event
 
-    new Event(name, location, startDate, endDate, startTime, endTime, desc, price, categories);
+    new Event(name, date, categories);
 
   }
 
@@ -91,43 +79,41 @@ class Eventonica {
   deleteEvent(id) {
     // user can click a button. an alert will confirm with the user that they want to delete the event. once deleted, the info for that object will be removed. 
     // Deletes Event
-    Event.all = Event.all.filter(Event[id]);
+    Event.all = Event.all.filter(event => event.id !== id);
   }
 
-  findEventsByDate(date) {
-    // user will be able to enter a starting date and an ending date; 
-    // user will be given results of events that fall between those dates.
-    // Return items in Event.all with a specified date
-    Event.all.filter(date => 
-      date > Event[startDate] && date < Event[endDate]);
-  }
+  // findEventsByDate(date) { // seems like duplicate in class Event
+  //   // user will be able to enter a starting date and an ending date; 
+  //   // user will be given results of events that fall between those dates.
+  //   // Return items in Event.all with a specified date
+  //   return Event.all.filter(event => event.date === date);
+  // }
 
-  findEventsbyCategory(category) {
-    // user will be able to view different categories --mapped to event property. upon clicking one, all events tagged in that category will be viewable in a list. all events will be actionable.
-    // try using strings to store the categories to place within an array or array of objects? 
-    // Return items in Event.all with a specified category
-    // have something that iterates over each checkbox rather than creating a variable for each.
-    Event.all.filter(category.includes(category) === true);
-  }
+  // findEventsbyCategory(category) { // seems like duplicate in class Event
+  //   // user will be able to view different categories --mapped to event property. upon clicking one, all events tagged in that category will be viewable in a list. all events will be actionable.
+  //   // try using strings to store the categories to place within an array or array of objects? 
+  //   // Return items in Event.all with a specified category
+  //   // have something that iterates over each checkbox rather than creating a variable for each.
+  //   return Event.all.filter(event => event.categories.includes(category));
+  // }
 
-  addUser(firstName, lastName, username, email, password) {
-    new User(firstName, lastName, username, email, password);
+  addUser(name) {
+    new User(name);
     // a user will be able to view and click a sign-up button and enter their info: name, email, password. user key will be generated via a private api. user info will be stored.
     // Adds a new User
 
   }
 
-  updateUser(id, prop, newProp) {
+  updateUser(id, newName) {
     // user will be able to edit info, such as email, name, and password. might require some confirmation actions. 
     // Update existing User
-    User[id][prop] = newProp.value; 
-
+    User.all.find(user => user.id === id).name = newName;
   }
 
   deleteUser(id) {
     // user will be able to view an option to delete their account in an account page. once deleted, user info can be deleted and unaccessable unless user wants to sign up again, 
     // Deletes User
-    User.all = User.all.filter(User[id]);
+    User.all = User.all.filter(user => user.id !== id);
 
   }
 }
