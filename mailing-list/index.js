2e272440b9ceb4e3ca23aa6ebbc26762bb94d434
Meia Natividad <meianatividad@gmail.com>
2021-03-03 16:27:51 -0800

    add solution

--- mailing-list/index.js
+++ mailing-list/index.js
@@ -3,27 +3,45 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 const lists = new Map();
+lists.set('yumbo', {members: ["talea@techtonica.org", "michelle@techtonica.org"]});
 
 // see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Map
 
 // add some fake data
 
 app.get('/lists', (req, res) => {
-  const listsArray = Array.from(lists.keys()); // Why is this like this? Try it out in your console.
+  const listsArray = Array.from(lists.keys());
+  // console.log("my lists: " + lists + listTest);
   res.send(listsArray);
 });
 
 app.route('/lists/:name').get((req,res)=>{
-  res.status(200).send(list);
-  res.status(404).send("Not found");
+  let list_name = req.params.name;
+  let status = 400;
+  let response = "Unable to fetch data!";
+  lists.forEach((list) => {
+      if (list["name"] == list_name) {
+          res.status(200).send(list);
+      }
+  });
+  res.status(status).send(response);
 });
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}...`);
 });
 
-const list = {
+
+// fake data 
+const staffList = {
   "name": "staff",
   "members": ["talea@techtonica.org", "michelle@techtonica.org"]
 }
 
+const participantList = {
+  "name": "participants",
+  "members": ["alex@techtonica.org", "taela@techtonica.org", "meia@techtonica.org"]
+}
+
+lists.set(`${staffList.name}`, staffList);
+lists.set(`${participantList.name}`, participantList);
