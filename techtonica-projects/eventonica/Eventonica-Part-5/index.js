c4bf0d400d97ace87ff07da2fcdf7712ece86d4e
Meia Natividad <meianatividad@gmail.com>
2021-03-04 20:36:09 -0800

    add import models.js line;

---
+++ techtonica-projects/eventonica/Eventonica-Part-5/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+
+// import addEvent from './models.js';
+const Eventonica = require('./models.js');
+// console.log(Example.test());
+// const main = require('./main.js');
+const app = express();
+const port = process.env.PORT || 3000;
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+// create a directory in eventonica called "server" and all server-related files should go there, and create another called "app". models --> server. 
+
+app.listen(port, () => {
+  console.log(`Server running port ${port}...`)
+})
+
+app.get('/', (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get('/events', (req, res) => {
+  res.send(Eventonica.Event.all);
+});
