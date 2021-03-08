248ce2c54e0865403094fb9ca70e7ad8fa929b9e
Meia Natividad <meianatividad@gmail.com>
2021-03-07 21:31:23 -0800

    add some post reqs

--- techtonica-projects/eventonica/Eventonica-Part-5/index.js
+++ techtonica-projects/eventonica/Eventonica-Part-5/index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 
 // import addEvent from './models.js';
-const models = require('./models.js');
+// const models = require('./models.js');
 // const eventonica = models.Eventonica();
-// const { Eventonica, User, Event, testEvent } = require('./models.js');
+const { User, Event, sky } = require('./models.js');
 // const eventonica = new Eventonica(); 
 
 const app = express();
@@ -25,19 +25,62 @@ app.get('/', (req, res) => {
 });
 
 app.route('/events').get((req, res) => {
-  // res.status(200).send(eventonica.Event.all);
   res.send(Event.all);
   console.log(Event.all);
+})
+
+// add a new event
+.post((req, res) => {
+  let newEvent = req.body;
+  Event.all.push(newEvent);
+  res.status(200).send(Event.all);
+  
 });
 
+// http://localhost:3000/events/100 returns the event with the specific id
+app.route('/events/:id').get((req, res) => 
+{ 
+  let event_id = req.params.id;
+  console.log(event_id, Event);
+  let status = 400;
+  let response = "Unable to fetch data!";
+  Event.all.forEach((event) => {
+    if (event['id'] == event_id) {
+      res.status(200).send(event);
+    }
+  })
+});
+
+// get all users 
 app.route('/users').get((req, res) => {
-  res.send(models.User.all);
+  res.send(User.all);
+})
+
+// add a new user, and return all users
+.post((req, res) => {
+  let newUser = req.body;
+  User.all.push(newUser);
+  res.status(200).send(User.all);
 });
 
+// http://localhost:3000/users/200 gets user with specific id
+app.route('/users/:id').get((req, res) => 
+{ 
+  let user_id = req.params.id;
+  let status = 400;
+  let response = "Unable to fetch data!";
+  User.all.forEach((user) => {
+    if (user['id'] == user_id) {
+      res.status(200).send(user);
+    }
+  })
+});
+
+
+// get specific user 
 app.route('/sky').get((req, res) => {
-  res.send(models.sky);
+  res.send(sky);
 });
 
-// app.route('/users/)
 
 // 
